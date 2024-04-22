a02262d8b41588e82663e364684b5abc2ce68229
pwa21 <pwa21@inf.ufpr.br>
2024-04-22 14:51:35 -0300

    finish step 1

--- poligonos.js
+++ poligonos.js
@@ -12,7 +12,7 @@ var lineLength = Math.sqrt(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 
 var midX = (startX + endX) / 2;
 var midY = (startY + endY) / 2;
 
-// Função para desenhar a reta e o marcador do meio
+// Função para desenhar a reta e os marcadores
 function drawLine() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
@@ -24,7 +24,17 @@ function drawLine() {
     midX = (startX + endX) / 2;
     midY = (startY + endY) / 2;
 
-    // Desenha o marcador do meio
+    // Desenha os marcadores
+    ctx.beginPath();
+    ctx.arc(startX, startY, 5, 0, 2 * Math.PI);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(endX, endY, 5, 0, 2 * Math.PI);
+    ctx.fillStyle = "green";
+    ctx.fill();
+
     ctx.beginPath();
     ctx.arc(midX, midY, 5, 0, 2 * Math.PI);
     ctx.fillStyle = "red";
@@ -35,6 +45,7 @@ drawLine(); // Desenha a reta inicialmente
 
 var dragging = false; // Flag para indicar se o mouse está sendo arrastado
 var dragStartX, dragStartY; // Variáveis para armazenar a posição inicial de arrasto
+var dragType; // Variável para indicar o tipo de marcador sendo arrastado
 
 // Evento de clique no canvas
 canvas.addEventListener('mousedown', function(e) {
@@ -43,15 +54,18 @@ canvas.addEventListener('mousedown', function(e) {
 
     // Verifica se o clique foi em um dos cantos da reta
     if (mouseX > startX - 5 && mouseX < startX + 5 && mouseY > startY - 5 && mouseY < startY + 5) {
-        dragging = 'start'; // Define que o canto de início está sendo arrastado
+        dragging = true;
+        dragType = 'start'; // Define que o marcador de início está sendo arrastado
         dragStartX = mouseX;
         dragStartY = mouseY;
     } else if (mouseX > endX - 5 && mouseX < endX + 5 && mouseY > endY - 5 && mouseY < endY + 5) {
-        dragging = 'end'; // Define que o canto de fim está sendo arrastado
+        dragging = true;
+        dragType = 'end'; // Define que o marcador de fim está sendo arrastado
         dragStartX = mouseX;
         dragStartY = mouseY;
     } else if (mouseX > midX - 5 && mouseX < midX + 5 && mouseY > midY - 5 && mouseY < midY + 5) {
-        dragging = 'line'; // Define que o marcador do meio está sendo arrastado
+        dragging = true;
+        dragType = 'line'; // Define que o marcador do meio está sendo arrastado
         dragStartX = mouseX;
         dragStartY = mouseY;
     }
@@ -64,22 +78,28 @@ canvas.addEventListener('mouseup', function(e) {
 
 // Evento de movimento do mouse
 canvas.addEventListener('mousemove', function(e) {
-    if (dragging === 'start') {
-        startX = e.pageX - this.offsetLeft;
-        startY = e.pageY - this.offsetTop;
-    } else if (dragging === 'end') {
-        endX = e.pageX - this.offsetLeft;
-        endY = e.pageY - this.offsetTop;
-    } else if (dragging === 'line') {
-        var offsetX = e.pageX - this.offsetLeft - dragStartX;
-        var offsetY = e.pageY - this.offsetTop - dragStartY;
-        startX += offsetX;
-        startY += offsetY;
-        endX += offsetX;
-        endY += offsetY;
-        dragStartX = e.pageX - this.offsetLeft;
-        dragStartY = e.pageY - this.offsetTop;
-    }
+    if (dragging) {
+        var mouseX = e.pageX - this.offsetLeft;
+        var mouseY = e.pageY - this.offsetTop;
+        
+        if (dragType === 'start') {
+            startX += mouseX - dragStartX;
+            startY += mouseY - dragStartY;
+        } else if (dragType === 'end') {
+            endX += mouseX - dragStartX;
+            endY += mouseY - dragStartY;
+        } else if (dragType === 'line') {
+            var offsetX = mouseX - dragStartX;
+            var offsetY = mouseY - dragStartY;
+            startX += offsetX;
+            startY += offsetY;
+            endX += offsetX;
+            endY += offsetY;
+        }
 
-    drawLine(); // Redesenha a reta após o movimento
+        dragStartX = mouseX;
+        dragStartY = mouseY;
+
+        drawLine(); // Redesenha a reta após o movimento
+    }
 });
